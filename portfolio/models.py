dda2fdb25868e03e0ebcd0cc8800ccf870503ea8
Ricardo Da Silva <dasilvaricardog@gmail.com>
2022-09-19 10:49:58 -0300

    Proyecto Ricardo Da Silva

--- portfolio/models.py
+++ portfolio/models.py
@@ -7,6 +7,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name='Titulo')
     description =models.TextField(verbose_name='Descripcion')   
     image =models.ImageField(verbose_name='Imagen', upload_to='projects')#todas las imagenes que se suban se van a guardar en la carpeta media/projects
+    link=models.URLField(verbose_name= 'Mas informacion',null=True,blank=True)
     created =models.DateTimeField(auto_now_add=True,verbose_name='Fecha de creacion')
     updated =models.DateTimeField(auto_now=True,verbose_name='Fecha de edicion') 
 
